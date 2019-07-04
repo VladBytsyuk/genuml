5b0a7f7b72349e7dbb81658dc8f26ad8259227b9
VladBytsyuk <vladislav.bytsyuk@gmail.com>
2019-07-04 23:11:50 +0300

    Remove unnecessary methods from use cases

--- src/main/com/vbytsyuk/genuml/usecases/Creating.kt
+++ src/main/com/vbytsyuk/genuml/usecases/Creating.kt
@@ -25,22 +25,6 @@ internal fun createElement(
     presentation = createElementPresentation(coordinate)
 )
 
-internal fun createInterface(name: String, coordinate: Coordinate) =
-    createElement(name, Element.Type.INTERFACE, coordinate)
-
-internal fun createOpenClass(name: String, coordinate: Coordinate) =
-    createElement(name, Element.Type.OPEN_CLASS, coordinate)
-
-internal fun createFinalClass(name: String, coordinate: Coordinate) =
-    createElement(name, Element.Type.FINAL_CLASS, coordinate)
-
-internal fun createAbstractClass(name: String, coordinate: Coordinate) =
-    createElement(name, Element.Type.ABSTRACT_CLASS, coordinate)
-
-internal fun createEnumClass(name: String, coordinate: Coordinate) =
-    createElement(name, Element.Type.ENUM_CLASS, coordinate)
-
-
 internal fun createElementPresentation(
     coordinate: Coordinate,
     style: Style.Element = DefaultStyle.Element
@@ -62,39 +46,6 @@ internal fun createReference(
     source = source, target = target,
     presentation = createArrowPresentation(start, end)
 )
-
-internal fun createReferenceAssociation(
-    source: Element,
-    target: Element,
-    start: Coordinate,
-    end: Coordinate
-) =
-    createReference(source, target, Reference.Type.ASSOCIATION, start, end)
-
-internal fun createReferenceAggregation(
-    source: Element,
-    target: Element,
-    start: Coordinate,
-    end: Coordinate
-) =
-    createReference(source, target, Reference.Type.AGGREGATION, start, end)
-
-internal fun createReferenceComposition(
-    source: Element,
-    target: Element,
-    start: Coordinate,
-    end: Coordinate
-) =
-    createReference(source, target, Reference.Type.COMPOSITION, start, end)
-
-internal fun createReferenceGeneralization(
-    source: Element,
-    target: Element,
-    start: Coordinate,
-    end: Coordinate
-) =
-    createReference(source, target, Reference.Type.GENERALIZATION, start, end)
-
 
 internal fun createArrowPresentation(
     start: Coordinate,
