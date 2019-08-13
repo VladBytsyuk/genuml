c46c12e02790465ae97ac68f6ca3e7d26a5ed50c
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2019-08-13 12:01:52 +0300

    Cleaned up code in tests

--- src/test/com/vbytsyuk/genuml/KotlinParserTest.kt
+++ src/test/com/vbytsyuk/genuml/KotlinParserTest.kt
@@ -2,7 +2,11 @@ package com.vbytsyuk.genuml
 
 import com.vbytsyuk.genuml.controllers.Parser
 import com.vbytsyuk.genuml.domain.Element
-import com.vbytsyuk.genuml.domain.Model
+import com.vbytsyuk.genuml.domain.Element.Type.FINAL_CLASS
+import com.vbytsyuk.genuml.domain.Element.Type.OPEN_CLASS
+import com.vbytsyuk.genuml.domain.Element.Type.INTERFACE
+import com.vbytsyuk.genuml.domain.Element.Type.ABSTRACT_CLASS
+import com.vbytsyuk.genuml.domain.Element.Type.ENUM_CLASS
 import com.vbytsyuk.genuml.parsers.*
 import org.junit.Test
 import kotlin.test.assertEquals
@@ -11,6 +15,99 @@ import kotlin.test.assertNotNull
 
 @Suppress("FunctionMaxLength", "TooManyFunctions", "UnsafeCast")
 class KotlinParserTest {
+
+    @Test
+    fun `0 source files, 0 elements`() = test(
+        sourceFilePaths = emptyList(),
+        parsedElements = emptyMap()
+    )
+
+    @Test
+    fun `1 source file, 0 elements`() = test(
+        sourceFilePaths = listOf(EMPTY_FILE),
+        parsedElements = emptyMap()
+    )
+
+    @Test
+    fun `1 source file, 1 interface`() = test(
+        sourceFilePaths = listOf(ONE_INTERFACE),
+        parsedElements = mapOf("Factory" to INTERFACE)
+    )
+
+    @Test
+    fun `1 source file, 1 final class`() = test(
+        sourceFilePaths = listOf(ONE_FINAL_CLASS),
+        parsedElements = mapOf("LastManOnEarth" to FINAL_CLASS)
+    )
+
+    @Test
+    fun `1 source file, 1 open class`() = test(
+        sourceFilePaths = listOf(ONE_OPEN_CLASS),
+        parsedElements = mapOf("Store" to OPEN_CLASS)
+    )
+
+    @Test
+    fun `1 source file, 1 abstract class`() = test(
+        sourceFilePaths = listOf(ONE_ABSTRACT_CLASS),
+        parsedElements = mapOf("Game" to ABSTRACT_CLASS)
+    )
+
+    @Test
+    fun `1 source file, 1 enum class`()  = test(
+        sourceFilePaths = listOf(ONE_ENUM_CLASS),
+        parsedElements = mapOf("Size" to ENUM_CLASS)
+    )
+
+    @Test
+    fun `1 source file, 2 same classes`() = test(
+        sourceFilePaths = listOf(TWO_SAME_CLASSES),
+        parsedElements = mapOf("Controller" to INTERFACE, "Interactor" to INTERFACE)
+    )
+
+    @Test
+    fun `1 source file, 2 different classes`() = test(
+        sourceFilePaths = listOf(TWO_DIFFERENT_CLASSES),
+        parsedElements = mapOf("Person" to OPEN_CLASS, "Phone" to FINAL_CLASS)
+    )
+
+    @Test
+    fun `2 source files, 1 open class`() = test(
+        sourceFilePaths = listOf(EMPTY_FILE, ONE_OPEN_CLASS),
+        parsedElements = mapOf("Store" to OPEN_CLASS)
+    )
+
+    @Test
+    fun `2 source files, 2 different classes`() = test(
+        sourceFilePaths = listOf(ONE_FINAL_CLASS, ONE_ENUM_CLASS),
+        parsedElements = mapOf("LastManOnEarth" to FINAL_CLASS, "Size" to ENUM_CLASS)
+    )
+
+    @Test
+    fun `2 source files, 3 different classes`() = test(
+        sourceFilePaths = listOf(TWO_DIFFERENT_CLASSES, ONE_ABSTRACT_CLASS),
+        parsedElements = mapOf("Person" to OPEN_CLASS, "Phone" to FINAL_CLASS, "Game" to ABSTRACT_CLASS)
+    )
+
+    @Test
+    fun `3 source files, 4 different classes`() = test(
+        sourceFilePaths = listOf(ONE_ENUM_CLASS, TWO_DIFFERENT_CLASSES, ONE_OPEN_CLASS),
+        parsedElements = mapOf("Person" to OPEN_CLASS, "Phone" to FINAL_CLASS, "Store" to OPEN_CLASS, "Size" to ENUM_CLASS)
+    )
+
+
+    private fun test(sourceFilePaths: List<String>, parsedElements: Map<String, Element.Type>) {
+        val parser = KotlinParser(SourceCodeReader())
+        val result = parser.parse(sourceFilePaths)
+        val actualModel = (result as Parser.Result.Success).model
+        assertEquals(actual = actualModel.references.size, expected = 0)
+        assertEquals(actual = actualModel.elements.size, expected = parsedElements.size)
+        parsedElements.forEach { (name, type) ->
+            val element = actualModel.elements.find { it.name == name }
+            assertNotNull(element)
+            assertEquals(actual = element?.type, expected = type)
+        }
+    }
+
 
     companion object {
         private const val DIRECTORY = "src/assets/kotlin_parser"
@@ -22,196 +119,5 @@ class KotlinParserTest {
         const val ONE_ENUM_CLASS = "$DIRECTORY/Size.kt"
         const val TWO_SAME_CLASSES = "$DIRECTORY/Arch.kt"
         const val TWO_DIFFERENT_CLASSES = "$DIRECTORY/Person.kt"
-    }
-
-    private fun test(paths: List<String>, block: (Parser.Result) -> Unit) {
-        val parser = KotlinParser(SourceCodeReader())
-        val result = parser.parse(paths)
-        block(result)
-    }
-
-    @Test
-    fun `0 source files, 0 elements`() = test(
-        paths = emptyList()
-    ) { result ->
-        val actualModel = (result as Parser.Result.Success).model
-        assertEquals(actual = actualModel, expected = Model())
-    }
-
-    @Test
-    fun `1 source file, 0 elements`() = test(
-        paths = listOf(EMPTY_FILE)
-    ) { result ->
-        val actualModel = (result as Parser.Result.Success).model
-        assertEquals(actual = actualModel, expected = Model())
-    }
-
-    @Test
-    fun `1 source file, 1 interface`() = test(
-        paths = listOf(ONE_INTERFACE)
-    ) { result ->
-        val actualModel = (result as Parser.Result.Success).model
-        assertEquals(actual = actualModel.references.size, expected = 0)
-        assertEquals(actual = actualModel.elements.size, expected = 1)
-        val element = actualModel.elements.first()
-        assertEquals(actual = element.name, expected = "Factory")
-        assertEquals(actual = element.type, expected = Element.Type.INTERFACE)
-    }
-
-    @Test
-    fun `1 source file, 1 final class`() = test(
-        paths = listOf(ONE_FINAL_CLASS)
-    ) { result ->
-        val actualModel = (result as Parser.Result.Success).model
-        assertEquals(actual = actualModel.references.size, expected = 0)
-        assertEquals(actual = actualModel.elements.size, expected = 1)
-        val element = actualModel.elements.first()
-        assertEquals(actual = element.name, expected = "LastManOnEarth")
-        assertEquals(actual = element.type, expected = Element.Type.FINAL_CLASS)
-    }
-
-    @Test
-    fun `1 source file, 1 open class`() = test(
-        paths = listOf(ONE_OPEN_CLASS)
-    ) { result ->
-        val actualModel = (result as Parser.Result.Success).model
-        assertEquals(actual = actualModel.references.size, expected = 0)
-        assertEquals(actual = actualModel.elements.size, expected = 1)
-        val element = actualModel.elements.first()
-        assertEquals(actual = element.name, expected = "Store")
-        assertEquals(actual = element.type, expected = Element.Type.OPEN_CLASS)
-    }
-
-    @Test
-    fun `1 source file, 1 abstract class`() = test(
-        paths = listOf(ONE_ABSTRACT_CLASS)
-    ) { result ->
-        val actualModel = (result as Parser.Result.Success).model
-        assertEquals(actual = actualModel.references.size, expected = 0)
-        assertEquals(actual = actualModel.elements.size, expected = 1)
-        val element = actualModel.elements.first()
-        assertEquals(actual = element.name, expected = "Game")
-        assertEquals(actual = element.type, expected = Element.Type.ABSTRACT_CLASS)
-    }
-
-    @Test
-    fun `1 source file, 1 enum class`()  = test(
-        paths = listOf(ONE_ENUM_CLASS)
-    ) { result ->
-        val actualModel = (result as Parser.Result.Success).model
-        assertEquals(actual = actualModel.references.size, expected = 0)
-        assertEquals(actual = actualModel.elements.size, expected = 1)
-        val element = actualModel.elements.first()
-        assertEquals(actual = element.name, expected = "Size")
-        assertEquals(actual = element.type, expected = Element.Type.ENUM_CLASS)
-    }
-
-    @Test
-    fun `1 source file, 2 same classes`() = test(
-        paths = listOf(TWO_SAME_CLASSES)
-    ) { result ->
-        val actualModel = (result as Parser.Result.Success).model
-        assertEquals(actual = actualModel.references.size, expected = 0)
-        assertEquals(actual = actualModel.elements.size, expected = 2)
-
-        val controllerInterface = actualModel.elements.find { it.name == "Controller" }
-        assertNotNull(controllerInterface)
-        assertEquals(actual = controllerInterface?.type, expected = Element.Type.INTERFACE)
-
-        val interactorInterface = actualModel.elements.find { it.name == "Interactor" }
-        assertNotNull(interactorInterface)
-        assertEquals(actual = interactorInterface?.type, expected = Element.Type.INTERFACE)
-    }
-
-    @Test
-    fun `1 source file, 2 different classes`() = test(
-        paths = listOf(TWO_DIFFERENT_CLASSES)
-    ) { result ->
-        val actualModel = (result as Parser.Result.Success).model
-        assertEquals(actual = actualModel.references.size, expected = 0)
-        assertEquals(actual = actualModel.elements.size, expected = 2)
-
-        val personClass = actualModel.elements.find { it.name == "Person" }
-        assertNotNull(personClass)
-        assertEquals(actual = personClass?.type, expected = Element.Type.OPEN_CLASS)
-
-        val phoneClass = actualModel.elements.find { it.name == "Phone" }
-        assertNotNull(phoneClass)
-        assertEquals(actual = phoneClass?.type, expected = Element.Type.FINAL_CLASS)
-    }
-
-    @Test
-    fun `2 source files, 1 open class`() = test(
-        paths = listOf(EMPTY_FILE, ONE_OPEN_CLASS)
-    ) { result ->
-        val actualModel = (result as Parser.Result.Success).model
-        assertEquals(actual = actualModel.references.size, expected = 0)
-        assertEquals(actual = actualModel.elements.size, expected = 1)
-        val element = actualModel.elements.first()
-        assertEquals(actual = element.name, expected = "Store")
-        assertEquals(actual = element.type, expected = Element.Type.OPEN_CLASS)
-    }
-
-    @Test
-    fun `2 source files, 2 different classes`() = test(
-        paths = listOf(ONE_FINAL_CLASS, ONE_ENUM_CLASS)
-    ) { result ->
-        val actualModel = (result as Parser.Result.Success).model
-        assertEquals(actual = actualModel.references.size, expected = 0)
-        assertEquals(actual = actualModel.elements.size, expected = 2)
-
-        val lastManOnEarthClass = actualModel.elements.find { it.name == "LastManOnEarth" }
-        assertNotNull(lastManOnEarthClass)
-        assertEquals(actual = lastManOnEarthClass?.type, expected = Element.Type.FINAL_CLASS)
-
-        val sizeClass = actualModel.elements.find { it.name == "Size" }
-        assertNotNull(sizeClass)
-        assertEquals(actual = sizeClass?.type, expected = Element.Type.ENUM_CLASS)
-    }
-
-    @Test
-    fun `2 source files, 3 different classes`() = test(
-        paths = listOf(TWO_DIFFERENT_CLASSES, ONE_ABSTRACT_CLASS)
-    ) { result ->
-        val actualModel = (result as Parser.Result.Success).model
-        assertEquals(actual = actualModel.references.size, expected = 0)
-        assertEquals(actual = actualModel.elements.size, expected = 3)
-
-        val personClass = actualModel.elements.find { it.name == "Person" }
-        assertNotNull(personClass)
-        assertEquals(actual = personClass?.type, expected = Element.Type.OPEN_CLASS)
-
-        val phoneClass = actualModel.elements.find { it.name == "Phone" }
-        assertNotNull(phoneClass)
-        assertEquals(actual = phoneClass?.type, expected = Element.Type.FINAL_CLASS)
-
-        val gameClass = actualModel.elements.find { it.name == "Game" }
-        assertNotNull(gameClass)
-        assertEquals(actual = gameClass?.type, expected = Element.Type.ABSTRACT_CLASS)
-    }
-
-    @Test
-    fun `3 source files, 4 different classes`() = test(
-        paths = listOf(ONE_ENUM_CLASS, TWO_DIFFERENT_CLASSES, ONE_OPEN_CLASS)
-    ) { result ->
-        val actualModel = (result as Parser.Result.Success).model
-        assertEquals(actual = actualModel.references.size, expected = 0)
-        assertEquals(actual = actualModel.elements.size, expected = 4)
-
-        val personClass = actualModel.elements.find { it.name == "Person" }
-        assertNotNull(personClass)
-        assertEquals(actual = personClass?.type, expected = Element.Type.OPEN_CLASS)
-
-        val phoneClass = actualModel.elements.find { it.name == "Phone" }
-        assertNotNull(phoneClass)
-        assertEquals(actual = phoneClass?.type, expected = Element.Type.FINAL_CLASS)
-
-        val gameClass = actualModel.elements.find { it.name == "Store" }
-        assertNotNull(gameClass)
-        assertEquals(actual = gameClass?.type, expected = Element.Type.OPEN_CLASS)
-
-        val sizeClass = actualModel.elements.find { it.name == "Size" }
-        assertNotNull(sizeClass)
-        assertEquals(actual = sizeClass?.type, expected = Element.Type.ENUM_CLASS)
     }
 }
