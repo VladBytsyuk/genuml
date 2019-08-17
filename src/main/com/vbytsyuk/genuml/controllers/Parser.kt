1401dc9e1e38ca1ccbbf554aff57c43fcf476234
VladBytsyuk <vladislav.bytsyuk@gmail.com>
2019-08-17 15:18:52 +0300

    Cleaned up Parser.kt

--- src/main/com/vbytsyuk/genuml/controllers/Parser.kt
+++ src/main/com/vbytsyuk/genuml/controllers/Parser.kt
@@ -16,49 +16,44 @@ abstract class Parser(private val sourceCodeReader: ISourceCodeReader) {
     abstract val extension: String
 
     fun parse(paths: List<String>): Result {
-        val parsedSources = paths
-            .map { path -> path to sourceCodeReader.readFile(path) }
-            .map { (path, sourceCode) -> path to parse(sourceCode, path) }
-            .toMap()
+        val parsedSources = paths.toPathWithParsingResult()
 
         val error = parsedSources.filterByResult<Result.Error>()
         val success = parsedSources.filterByResult<Result.Success>()
 
-        if (error.isNotEmpty()) {
-            val errorPaths = error.mapToPaths()
-            return Result.Error("Can't parse sources: $errorPaths")
+        return when {
+            error.isNotEmpty() -> Result.Error("Can't parse sources: ${error.mapToPaths()}")
+            else -> success.mapToModels().merge()
         }
-
-        return success.mapToResults().mapToModel().merge()
     }
 
-    internal abstract fun parseFile(sourceCodeLines: List<String>): Result
+
+    private fun List<String>.toPathWithParsingResult(): Map<String, Result> =
+        this.map { path -> path to sourceCodeReader.readFile(path) }
+            .map { (path, sourceCode) -> path to parse(sourceCode, path) }
+            .toMap()
 
     private fun parse(sourceCode: List<String>, path: String): Result = when {
         path.endsWith(extension) -> parseFile(sourceCode)
         else -> Result.WrongExtension
     }
+
+    internal abstract fun parseFile(sourceCodeLines: List<String>): Result
 }
 
 
 private inline fun <reified R : Parser.Result> Map<String, Parser.Result>.filterByResult(): Map<String, R> =
-    mapNotNull { (path, result) ->
-        when (result) {
-            is R -> path to result
-            else -> null
-        }
-    }.toMap()
+    mapNotNull { (path, result) -> if (result is R) path to result else null }.toMap()
+
 
 private inline fun <reified R : Parser.Result> Map<String, R>.mapToPaths() =
     map { (path, _) -> path }
 
-private inline fun <reified R : Parser.Result> Map<String, R>.mapToResults() =
-    map { (_, result) -> result }
+private fun Map<String, Parser.Result.Success>.mapToModels() =
+    map { (_, result) -> result.model }
 
-private fun List<Parser.Result.Success>.mapToModel() = map { it.model }
 
-private fun List<Model>.merge(): Parser.Result =
-    when (val modelMergerResult = ModelMerger.merge(this)) {
-        is ModelMerger.Result.Success -> Parser.Result.Success(modelMergerResult.model)
-        ModelMerger.Result.Error -> Parser.Result.Error("Source files have name clashing. Incorrect source code.")
-    }
+private fun List<Model>.merge(): Parser.Result = when (val modelMergerResult = ModelMerger.merge(this)) {
+    is ModelMerger.Result.Success -> Parser.Result.Success(modelMergerResult.model)
+    is ModelMerger.Result.Error -> Parser.Result.Error("Source files have name clashing. Incorrect source code.")
+}
