a732251da0ea63e1bb14c5c1a1718221095010fc
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2020-07-29 23:53:53 +0300

    Simple elements UI

--- ui/src/main/kotlin/com/vbytsyuk/genuml/ui/screens/editor/CanvasExtensions.kt
+++ ui/src/main/kotlin/com/vbytsyuk/genuml/ui/screens/editor/CanvasExtensions.kt
@@ -1,22 +1,53 @@
 package com.vbytsyuk.genuml.ui.screens.editor
 
+import com.vbytsyuk.genuml.domain.Element
+import com.vbytsyuk.genuml.domain.VisibilityModifier
 import javafx.scene.canvas.Canvas
 import javafx.scene.paint.Color
 import javafx.scene.paint.Paint
 
 
 @SuppressWarnings("MagicNumber")
-fun Canvas.renderElements(elements: List<String>?) {
+fun Canvas.renderElements(elements: List<Element>?) {
     graphicsContext2D.clearRect(0.0, 0.0, WIDTH, HEIGHT)
-    var x = 0.0
+    val x = 0.0
     var y = 40.0
-    val colors = listOf(Color.AQUA, Color.BEIGE, Color.VIOLET, Color.CADETBLUE, Color.YELLOWGREEN)
-    elements?.forEach { elementName ->
-        drawRect(x, y, width = 250.0, height = 50.0, fillColor = colors.random(), strokeWidth = 1.0)
-        drawText(x + 25.0, y + 10, text = elementName)
-        x += 15.0
-        y += 55.0
+    elements?.forEach { element ->
+        drawRect(x, y, width = 450.0, height = 250.0, fillColor = element.color, strokeWidth = 1.0)
+        drawText(x + 25.0, y + 10, text = element.name)
+        y += 50.0
+        element.properties.forEach { property ->
+            val visibility = when (property.visibilityModifier) {
+                VisibilityModifier.PRIVATE -> '-'
+                VisibilityModifier.PUBLIC -> '+'
+                VisibilityModifier.PROTECTED -> '#'
+                VisibilityModifier.INTERNAL -> '~'
+            }
+            drawText(x + 25.0, y + 10, text = "$visibility\t${property.name}: ${property.type}")
+            y += 25.0
+        }
+        y += 25.0
+        element.methods.forEach { meth ->
+            val visibility = when (meth.visibilityModifier) {
+                VisibilityModifier.PRIVATE -> '-'
+                VisibilityModifier.PUBLIC -> '+'
+                VisibilityModifier.PROTECTED -> '#'
+                VisibilityModifier.INTERNAL -> '~'
+            }
+            val args = meth.arguments.joinToString(separator = ", ") { "${it.name}: ${it.type}" }
+            drawText(x + 25.0, y + 10, text = "$visibility\t${meth.name}($args): ${meth.returnType}")
+            y += 25.0
+        }
+        y += 250.0
     }
+}
+
+private val Element.color: Color get() = when (this.type) {
+    Element.Type.INTERFACE -> Color.MEDIUMSEAGREEN
+    Element.Type.FINAL_CLASS -> Color.CORNFLOWERBLUE
+    Element.Type.OPEN_CLASS -> Color.AQUA
+    Element.Type.ABSTRACT_CLASS -> Color.BURLYWOOD
+    Element.Type.ENUM_CLASS -> Color.BLUEVIOLET
 }
 
 
